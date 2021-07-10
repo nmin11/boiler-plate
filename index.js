1180e250042f559404903e892345652d12bea76c
nmin11 <nmin1124@gmail.com>
2021-07-10 18:41:29 +0900

    Implement Login & Logout with Authentication

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const config = require('./config/key')
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -19,7 +21,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => { res.send('Hi') })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     //회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
     const user = new User(req.body)
     user.save((err, userInfo) => {
@@ -27,6 +29,58 @@ app.post('/register', (req, res) => {
         return res.status(200).json({
             success: true
         })
+    })
+})
+
+app.post('/api/users/login', (req, res) => {
+    //요청된 이메일이 DB에 있는지 확인
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+            }
+            //비밀번호까지 맞다면 토큰을 생성하기
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                //토큰을 쿠키나 로컬 스토리지에 저장한다. 여기에서는 쿠키에 저장한다.
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    //여기까지 middleware를 통과해 왔다는 것은 Authentication이 true라는 뜻
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" },
+        (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
     })
 })
 
