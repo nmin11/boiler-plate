7d1d85737e409090b9820e360fcb3e40a71ebf31
nmin11 <nmin1124@gmail.com>
2021-07-10 13:44:37 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://loko:dd082082@cluster0.amkeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {res.send('Hello World!')})
+
+app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
