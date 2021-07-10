51b6a6beddbf64178f01730601498a0c7967647b
nmin11 <nmin1124@gmail.com>
2021-07-10 17:05:57 +0900

    Edit mongoDB setting

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://loko:dd082082@cluster0.amkeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err))
+    .catch(err => console.log(err))
 
-app.get('/', (req, res) => {res.send('Hello World!')})
+app.get('/', (req, res) => { res.send('Hi') })
+
+app.post('/register', (req, res) => {
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
